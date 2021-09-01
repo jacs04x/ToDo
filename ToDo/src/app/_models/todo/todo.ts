b5832a0bebfec44ec66130fb4df06cc323a6e548
jacs04x <30944431+jacs04x@users.noreply.github.com>
2021-09-01 17:23:10 -0500

    first

---
+++ ToDo/src/app/_models/todo/todo.ts
@@ -0,0 +1,12 @@
+export class Todo {
+
+    nombre: String
+    descripcion: String
+
+
+    constructor(nombre, descripcion){
+        this.nombre = nombre;
+        this.descripcion = descripcion
+    }
+
+}
