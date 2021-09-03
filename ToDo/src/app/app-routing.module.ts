f5b9041ed0017e3fc041f2d5e2e1757b06d73e19
jacs04x <30944431+jacs04x@users.noreply.github.com>
2021-09-03 13:42:18 -0500

    update

--- ToDo/src/app/app-routing.module.ts
+++ ToDo/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
+import {AboutComponent} from './components/about/about.component';
+const routes: Routes = [
+  {path : '', component: TodoComponent},
+  {path: 'about', component: AboutComponent},
+  { path: '**', component: TodoComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ToDo/src/app/app.module.ts
+++ ToDo/src/app/app.module.ts
@@ -4,16 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    FooterComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
+  exports: [
+    AboutComponent,
+    FooterComponent,
+    NavbarComponent,
+    TodoComponent
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
